760a83973fcddce5a5e215aa173888fa1e49b68e
Joaquin <128936304+re-Silent@users.noreply.github.com>
2025-04-07 20:17:53 +0300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <span>REFRESH. Документация</span>,
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'REFRESH LLC. 2025. Все права защищены.',
   },
 }
 
